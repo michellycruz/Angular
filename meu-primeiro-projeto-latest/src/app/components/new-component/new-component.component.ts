0c05920eb03769e47605ed9ba06486ae2b02f227
Mich <michxcrz@gmail.com>
2024-01-04 17:08:26 -0300

    Update new-component.component.ts

--- meu-primeiro-projeto-latest/src/app/components/new-component/new-component.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/new-component/new-component.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './new-component.component.html',
   styleUrl: './new-component.component.scss'
 })
