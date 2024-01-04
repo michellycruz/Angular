a04fa644addb013ef5376560065619fff7d46dd1
Mich <michxcrz@gmail.com>
2024-01-04 16:39:12 -0300

    novo componente

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NewComponent],
   template: `
   <!-- <router-outlet></router-outlet> -->
   <h1>Curso de Angular</h1>
-  <h2>Components</h2>
+  <app-new-component />
   `
 })
 export class AppComponent {
